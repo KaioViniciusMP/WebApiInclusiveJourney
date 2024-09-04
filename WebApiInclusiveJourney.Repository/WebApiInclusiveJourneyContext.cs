b4138477374f1e251baea7f7d2b6ec859c0c92ab
Kaio <Kaio@DESKTOP-VI3KO6V>
2024-09-03 21:56:54 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiInclusiveJourney.Repository/WebApiInclusiveJourneyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiInclusiveJourney.Repository.Models;
+
+namespace WebApiInclusiveJourney.Repository
+{
+    public class WebApiInclusiveJourneyContext : DbContext
+    {
+        public WebApiInclusiveJourneyContext(DbContextOptions<WebApiInclusiveJourneyContext> options) : base(options) { }
+
+        public DbSet<TabUsuario> tabUsuario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TabUsuario>().ToTable("TabUsuario");
+            modelBuilder.Entity<TabUsuario>();
+        }
+    }
+}
